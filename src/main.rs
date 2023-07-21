4d0d8d61c9651ec7c46acfccfc31f80ccbd69268
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-07-20 23:22:09 -0600

    Simple jump to kernel main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use core::{arch::global_asm, hint::black_box, panic::PanicInfo};
 
 // Loads our entry point, _start, written entirely in assembly
 global_asm!(include_str!("start.S"));
 
 #[no_mangle]
-pub extern "C" fn main() -> ! {
+pub extern "C" fn main(dead_beef: u64) -> ! {
+    // Debug statements to verify main function is executing with qemu 'info registers'
+    let mut _db = dead_beef;
+    black_box(_db = _db - 0xbeef);
+    black_box(_db);
+
+    // Never return from this divering fn
     loop {}
 }
 
