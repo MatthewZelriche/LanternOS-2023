4bb32806b8a78bcd1174614b9cc39e5d052437b2
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-07-23 19:58:39 -0600

    Implement simple kprint utility

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,16 @@
 mod concurrency;
 mod mmio;
 mod peripherals;
+mod util;
 
-use bitfield::Bit;
-use core::{arch::global_asm, hint, panic::PanicInfo};
-use mmio::{mmio_read, mmio_write};
+use core::arch::global_asm;
 
 // Loads our entry point, _start, written entirely in assembly
 global_asm!(include_str!("start.S"));
 
 #[no_mangle]
 pub extern "C" fn main() -> ! {
-    kprint("Hello, world!\n");
-
+    kprint!("Hello world! My number is: {}", 7);
     // Never return from this diverging fn
-    panic!()
-}
-
-fn kputc(c: char) {
-    while mmio_read(mmio::UART_FR).bit(5) {
-        hint::spin_loop();
-    }
-    mmio_write(mmio::UART0_BASE, c as u64);
-}
-
-fn kprint(s: &str) {
-    for c in s.chars() {
-        kputc(c);
-    }
-}
-
-// Just spin the cpu...cant do anything else right now.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    kprint("Kernel panic!");
-    loop {}
+    panic!("Reached end of kmain!")
 }
---
+++ src/util.rs
@@ -0,0 +1,17 @@
+use core::panic::PanicInfo;
+
+#[macro_export]
+macro_rules! kprint {
+    ($($arg:tt)*) => {
+        use core::fmt::Write;
+        use crate::peripherals::UART;
+        writeln!(UART.lock(), $($arg)*).unwrap();
+    };
+}
+
+// Just spin the cpu...cant do anything else right now.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    kprint!("{}", _info);
+    loop {}
+}
