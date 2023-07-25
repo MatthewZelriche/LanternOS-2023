455fb28383ec0ced7bb94183dcc6d82d182ce129
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-07-25 11:07:51 -0600

    UART init
    * Currently untested due to lack of access to real hardware

--- src/mmio.rs
+++ src/mmio.rs
@@ -6,10 +6,10 @@ pub const _GPPUDCLK0: u32 = GPIO_BASE + 0x98;
 
 pub const UART0_BASE: u32 = GPIO_BASE + 0x1000;
 pub const UART_FR: u32 = UART0_BASE + 0x18;
-pub const _UART0_IBRD: u32 = UART0_BASE + 0x24;
-pub const _UART0_FBRD: u32 = UART0_BASE + 0x28;
-pub const _UART0_LCR_H: u32 = UART0_BASE + 0x2C;
-pub const _UART0_CR: u32 = UART0_BASE + 0x30;
+pub const UART0_IBRD: u32 = UART0_BASE + 0x24;
+pub const UART0_FBRD: u32 = UART0_BASE + 0x28;
+pub const UART0_LCR_H: u32 = UART0_BASE + 0x2C;
+pub const UART0_CR: u32 = UART0_BASE + 0x30;
 pub const _UART0_IMSC: u32 = UART0_BASE + 0x38;
 pub const _UART0_ICR: u32 = UART0_BASE + 0x44;
 
--- src/peripherals/uart.rs
+++ src/peripherals/uart.rs
@@ -1,20 +1,56 @@
 pub struct Uart;
 
-use crate::mmio::{self, mmio_read, mmio_write};
-use bitfield::Bit;
-use core::{
-    fmt::{Result, Write},
-    hint,
-};
+use crate::mmio::{self, mmio_read, mmio_write, UART0_CR, UART0_FBRD, UART0_IBRD, UART0_LCR_H};
+use bitfield::{Bit, BitMut};
+use core::{fmt::Write, hint};
+
+use super::{mailbox::SetClockRate, MAILBOX};
 
 impl Uart {
+    const INIT_RATE_DEF: u32 = 3000000;
     pub fn new() -> Self {
-        let instance = Uart {};
-        instance.flush_fifo();
-
-        // TODO: proper init
+        let mut instance = Uart {};
+        // Unwrap here since it would be fatal for this initialization to fail
+        // before we have a functional way of outputting text
+        // TODO: May be able to change this once we get framebuffer working
+        instance.reset(Uart::INIT_RATE_DEF).unwrap();
 
         instance
+    }
+
+    pub fn reset(&mut self, clock_rate: u32) -> Result<(), ()> {
+        // TODO: Currently untested due to lack of access to real hardware
+
+        // Wait until buffered output is finished
+        self.flush_fifo();
+
+        // Shut down UART0
+        mmio_write(UART0_CR, 0);
+
+        // Set UART clock rate
+        let msg = SetClockRate::new(2, clock_rate);
+        let _ = MAILBOX.lock().send_message(msg)?;
+
+        let baud: u32 = clock_rate / 16;
+        let baud_whole: u32 = clock_rate / (16 * baud);
+        let baud_frac: u32 = clock_rate % (16 * baud);
+        mmio_write(UART0_IBRD, baud_whole);
+        mmio_write(UART0_FBRD, baud_frac);
+
+        let mut lcr: u32 = 0;
+        lcr.set_bit(4, true);
+        lcr.set_bit(5, true);
+        lcr.set_bit(6, true);
+        mmio_write(UART0_LCR_H, lcr);
+
+        // Bring the UART0 back online
+        let mut cr_data: u32 = 0;
+        cr_data.set_bit(0, true);
+        cr_data.set_bit(8, true);
+        cr_data.set_bit(9, true);
+        mmio_write(UART0_CR, cr_data);
+
+        Ok(())
     }
 
     fn flush_fifo(&self) {
