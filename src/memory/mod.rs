f24d4bf2a32ac492aa1cd79e0090e165a6647773
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-07-26 22:27:26 -0600

    Fix get_page_addr bug

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -4,6 +4,7 @@ pub mod util;
 // Size of pages, in bytes
 const PAGE_SZ: u64 = 0x1000;
 
+// TODO: Dehardcode this for other page sizes
 pub fn get_page_addr(addr: u64) -> u64 {
-    addr & !(PAGE_SZ)
+    addr & !(PAGE_SZ - 1)
 }
