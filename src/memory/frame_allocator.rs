fda3e0db3dfb7ccae9852a51ce57401407b8202d
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-07-30 09:22:18 -0600

    Finish physical frame allocator

--- src/memory/frame_allocator.rs
+++ src/memory/frame_allocator.rs
@@ -22,10 +22,7 @@ impl PageFrameAllocator<'_> {
                 super::map::EntryType::Free => {
                     for addr in (entry.base_addr..entry.end_addr).step_by(PAGE_SZ as usize) {
                         // If we fail to add a page to the free list, just silently ignore
-                        match allocator.append_free_page(addr as *mut u64) {
-                            Ok(_) => allocator.num_free += 1,
-                            Err(_) => (),
-                        }
+                        let _ = allocator.free_page(addr as *mut u64);
                     }
                 }
                 _ => (),
@@ -39,17 +36,17 @@ impl PageFrameAllocator<'_> {
         self.num_free
     }
 
-    fn append_free_page(&mut self, frame_addr: *mut u64) -> Result<(), ()> {
+    pub fn free_page(&mut self, frame_addr: *mut u64) -> Result<(), ()> {
         // Can't use null address (0x0) as a valid page, even though ARM bare metal would
         // allow us to do so.
+        assert!(frame_addr as u64 % PAGE_SZ == 0);
         if frame_addr.is_null() {
             return Err(());
         }
 
-        // Appends to the front of the linked list
-        let new_node = Some(Node {
+        let new_node = Node {
             next: self.freelist,
-        });
+        };
 
         // We store the freelist nodes "in-place". Each free page is by definition not being used
         // for anything else, so we can utilize it without having to allocate any additional memory to
@@ -59,14 +56,27 @@ impl PageFrameAllocator<'_> {
         // size is 4 KiB.
         // The pointer is the start of a free page, so we know that we aren't damaging any existing data
         // and so this pointer is always valid for writes.
-        // We convert the pointer to a reference immediately after writing an Option to the pointer, so we
-        // know the pointer can be converted to a &Option
-        let addr_option_ptr = frame_addr as *mut Option<Node>;
+        // We convert the pointer to a reference immediately after writing a Node to the pointer, so we
+        // know the pointer can be converted to a &Node
+        let addr_option_ptr = frame_addr as *mut Node;
         unsafe {
             core::ptr::write(addr_option_ptr, new_node);
-            self.freelist = addr_option_ptr.as_ref().unwrap().as_ref();
+            self.freelist = Some(addr_option_ptr.as_ref().unwrap());
         }
 
+        self.num_free += 1;
         Ok(())
+    }
+
+    pub fn alloc_page(&mut self) -> Result<*mut u64, ()> {
+        match self.freelist {
+            Some(head) => {
+                let addr = head as *const Node as *mut u64;
+                self.freelist = head.next;
+                self.num_free -= 1;
+                Ok(addr)
+            }
+            None => Err(()),
+        }
     }
 }
