2dcfcc9efe600c2ecdfb05fd96a5d9c6780eb160
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-08-10 13:46:57 -0600

    Implement mutex with ARM64 primitives

--- libs/arch/raspi/concurrency/src/mutex.rs
+++ libs/arch/raspi/concurrency/src/mutex.rs
@@ -1,22 +1,66 @@
+use core::arch::asm;
+
 use lock_api::GuardSend;
 
 pub struct RawMutex(u64);
 
 unsafe impl lock_api::RawMutex for RawMutex {
     const INIT: Self = RawMutex(0);
-
     type GuardMarker = GuardSend;
 
     fn lock(&self) {
-        todo!()
+        unsafe {
+            asm!(
+                "2:",
+                "ldr x9, =1",
+                "LDXR x10, [{lock_ptr}]",
+                "cmp x10, #1",                  // Test if the mutex is currently locked
+                "beq 3f",                       // If locked, wait until we can try again later
+                "stxr w11, x9, [{lock_ptr}]",   // Otherwise, attempt a lock
+                "cmp w11, #0",                  // Did the lock succeed?
+                "beq 4f",                       // Then jump to the end
+                "",
+                "3:",                           // Couldn't attain a lock...
+                "wfe",                          // Sleep CPU until we can try again
+                "b 2b",
+                "4:",                           // Successful lock, prevent re-ordering & exit
+                "DMB SY",
+                lock_ptr = in(reg) (&self.0) as *const u64,
+            );
+        }
     }
 
     fn try_lock(&self) -> bool {
-        todo!()
+        let mut res: u32 = 1;
+        unsafe {
+            asm!(
+                "ldr x9, =1",
+                "LDXR x10, [{lock_ptr}]",
+                "cmp x10, #1",                  // Test if the mutex is currently locked
+                "beq 2f",                       // If locked, wait until we can try again later
+                "stxr w11, x9, [{lock_ptr}]",   // Otherwise, attempt a lock
+                "mov {res:w}, w11",             // Did we succeed at getting the lock?
+                "DMB SY",
+                "2:",
+                lock_ptr = in(reg) (&self.0) as *const u64,
+                res = inout(reg) res,
+            );
+        }
+
+        res == 0
     }
 
     unsafe fn unlock(&self) {
-        todo!()
+        unsafe {
+            asm!(
+            "DMB SY",
+            "ldr x9, =0",
+            "str x9, [{lock_ptr}]",             // Unlock the mutex.
+            "sev",                              // Inform others that they can attempt to
+                                                // lock the mutex again
+            lock_ptr = in(reg) (&self.0) as *const u64,
+            );
+        }
     }
 }
 
