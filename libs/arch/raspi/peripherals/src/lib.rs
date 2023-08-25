584c6c300ae36d5f2ba7f1d9e7eda89f50814af3
MatthewZelriche <67518084+MatthewZelriche@users.noreply.github.com>
2023-08-25 14:16:36 -0600

    Update peripheral constants

--- libs/arch/raspi/peripherals/src/lib.rs
+++ libs/arch/raspi/peripherals/src/lib.rs
@@ -11,7 +11,8 @@ impl ConstantsRaspi3 {
     pub const PERIPHERALS_PHYS_BASE: u64 = 0x3F000000; // Raspi3
     pub const PERIPHERALS_PHYS_END: u64 = 0x40000000; //Raspi3
     pub const MMIO_OFFSET: u64 =
-        ConstantsRaspi3::MMIO_PHYS_BASE - ConstantsRaspi3::PERIPHERALS_PHYS_BASE; // Raspi3
+        ConstantsRaspi3::MMIO_PHYS_BASE - ConstantsRaspi3::PERIPHERALS_PHYS_BASE;
+    pub const EMMC_OFFSET_FROM_MMIO_BASE: u64 = 0x300000;
 }
 
 pub struct ConstantsRaspi4;
@@ -20,45 +21,64 @@ impl ConstantsRaspi4 {
     pub const PERIPHERALS_PHYS_END: u64 = 0x100000000; // Raspi4
     pub const MMIO_PHYS_BASE: u64 = 0xFE000000; // Raspi4
     pub const MMIO_OFFSET: u64 =
-        ConstantsRaspi4::MMIO_PHYS_BASE - ConstantsRaspi4::PERIPHERALS_PHYS_BASE; // Raspi4
+        ConstantsRaspi4::MMIO_PHYS_BASE - ConstantsRaspi4::PERIPHERALS_PHYS_BASE;
+    // Sourced from: https://github.com/librerpi/rpi-open-firmware/blob/master/docs/rpi4-gpio-mux.dot
+    // Seems very poorly documented
+    pub const EMMC_OFFSET_FROM_MMIO_BASE: u64 = 0x340000;
 }
 
-pub fn get_default_mmio_base() -> u64 {
+pub enum Board {
+    RPI3,
+    RPI4,
+    UNSUPPORTED,
+}
+
+pub fn get_board() -> Board {
     let val = aarch64_cpu::registers::MIDR_EL1.get();
     let partno: u64 = val.bit_range(15, 4);
 
     match partno {
-        0xD03 => ConstantsRaspi3::MMIO_PHYS_BASE,
-        0xD08 => ConstantsRaspi4::MMIO_PHYS_BASE,
-        _ => panic!("Unsupported board type"),
+        0xD03 => Board::RPI3,
+        0xD08 => Board::RPI4,
+        _ => Board::UNSUPPORTED,
+    }
+}
+
+pub fn get_default_mmio_base() -> u64 {
+    match get_board() {
+        Board::RPI3 => ConstantsRaspi3::MMIO_PHYS_BASE,
+        Board::RPI4 => ConstantsRaspi4::MMIO_PHYS_BASE,
+        Board::UNSUPPORTED => panic!("Unsupported board type"),
     }
 }
 
 pub fn get_board_peripheral_range() -> (u64, u64) {
-    let val = aarch64_cpu::registers::MIDR_EL1.get();
-    let partno: u64 = val.bit_range(15, 4);
-
-    match partno {
-        0xD03 => (
+    match get_board() {
+        Board::RPI3 => (
             ConstantsRaspi3::PERIPHERALS_PHYS_BASE,
             ConstantsRaspi3::PERIPHERALS_PHYS_END,
         ),
-        0xD08 => (
+        Board::RPI4 => (
             ConstantsRaspi4::PERIPHERALS_PHYS_BASE,
             ConstantsRaspi4::PERIPHERALS_PHYS_END,
         ),
-        _ => panic!("Unsupported board type"),
+        Board::UNSUPPORTED => panic!("Unsupported board type"),
     }
 }
 
 pub fn get_mmio_offset_from_peripheral_base() -> u64 {
-    let val = aarch64_cpu::registers::MIDR_EL1.get();
-    let partno: u64 = val.bit_range(15, 4);
+    match get_board() {
+        Board::RPI3 => ConstantsRaspi3::MMIO_OFFSET,
+        Board::RPI4 => ConstantsRaspi4::MMIO_OFFSET,
+        Board::UNSUPPORTED => panic!("Unsupported board type"),
+    }
+}
 
-    match partno {
-        0xD03 => ConstantsRaspi3::MMIO_OFFSET,
-        0xD08 => ConstantsRaspi4::MMIO_OFFSET,
-        _ => panic!("Unsupported board type"),
+fn get_emmc_offset_from_mmio_base() -> u64 {
+    match get_board() {
+        Board::RPI3 => ConstantsRaspi3::EMMC_OFFSET_FROM_MMIO_BASE,
+        Board::RPI4 => ConstantsRaspi4::EMMC_OFFSET_FROM_MMIO_BASE,
+        Board::UNSUPPORTED => panic!("Unsupported board type"),
     }
 }
 
